4fa9a9d8d60e1c693b8f9c8037b61bd2e2509a02
钩钩么么哒 <a516550257@qq.com>
2015-10-25 12:16:18 +0800

    图标更新

--- background.js
+++ background.js
@@ -2,11 +2,11 @@ var clicked = false
 chrome.browserAction.onClicked.addListener(function(tab) {
 	if (clicked == false){
 		chrome.browserAction.setIcon({path:"icon.png"})
-		chrome.tabs.executeScript(null, {file:"content.js"})
+		chrome.tabs.executeScript(null, {file:"hide.js"})
 		clicked = true
 	}else{
 		chrome.browserAction.setIcon({path:"icon1.png"})
-		chrome.tabs.executeScript(null, {code:"document.body.bgColor='white'"})
+		chrome.tabs.executeScript(null, {file:"resume.js"})
 		clicked = false
 	}
 });
--- content.js
+++
@@ -1,7 +0,0 @@
-/*if (document.body.bgColor == 'white'){
-	document.body.bgColor='red'
-}else{
-	document.body.bgColor='white'
-}*/
-
-document.body.bgColor='red'
