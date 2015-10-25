513e99197e174776d34c2471676de4840cd87774
钩钩么么哒 <a516550257@qq.com>
2015-10-25 14:15:10 +0800

    发布版本

--- background.js
+++ background.js
@@ -1,12 +1,4 @@
-var clicked = false
 chrome.browserAction.onClicked.addListener(function(tab) {
-	if (clicked == false){
-		chrome.browserAction.setIcon({path:"icon.png"})
-		chrome.tabs.executeScript(null, {file:"hide.js"})
-		clicked = true
-	}else{
-		chrome.browserAction.setIcon({path:"icon1.png"})
-		chrome.tabs.executeScript(null, {file:"resume.js"})
-		clicked = false
-	}
+	chrome.tabs.executeScript(null, {file:"jquery.js"})
+	chrome.tabs.executeScript(null, {file:"hide.js"})
 });
