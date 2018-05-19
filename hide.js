aee96fb715387d0b1321a4660127b01ff49279bc
钩钩么么哒 <a516550257@qq.com>
2018-05-20 01:31:15 +0900

    更新

--- hide.js
+++ hide.js
@@ -1,32 +1,11 @@
-$(".zm-editable-content").each(function(){
+$(".RichContent-inner").each(function(){
 	if ($(this).find("img").length == 0){
 		$(this).css("display", "none")
 	}else{
-		var content = $(this).html()
-		var startPos = -1;
-		var realContent = ""
-		while(true){
-			var startPos = content.indexOf('<', startPos + 1)
-			if (content.substr(startPos + 1, 8) == "noscript"){
-				var begin = startPos
-				while (true){
-					startPos = content.indexOf('<', startPos + 1)
-					if (content.substr(startPos + 1, 3) == "img"){
-						var endPos = content.indexOf('>', startPos + 1)
-						console.log(endPos + " end")
-						realContent += content.substring(begin, endPos + 1)
-						break;
-					}
-				}
-			}
-			if (startPos == -1){
-				$(this).html(realContent)
-				break;
-			}
-		}
-		$(this).find("img").each(function(){
-			var src = $(this).attr("data-actualsrc")
-			$(this).attr("src", src)
+		var content = "";
+		$(this).find("img").each(function(index, img) {
+			content += img.outerHTML;
 		})
+		$(this).html(content);
 	}
 })
